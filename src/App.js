560976408b785fa5f055a418d0221d8b8f0cca82
Kiril <kaloyanov.kiril@gmail.com>
2023-02-09 10:58:22 +0200

    Styled the sidebar and default button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from './buttonComponent.js';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className={styles.App}>
+      <div>
+        <aside>
+          <header>
+            <span>Dev</span>Challenges
+          </header>
+          <div>Colors</div>
+          <div>Typography</div>
+          <div>Spaces</div>
+          <div className={styles.activeText}>Buttons</div>
+          <div>Inputs</div>
+          <div>Grid</div>
+        </aside>
+      </div>
+      <div>
+        <Button variant='outline' />
+        <footer><a href="https://devchallenges.io/portfolio/KirilKaloyanov" target={'_blank'}>created by <strong>KirilKaloyanov</strong> - devChallenges.io</a></footer>
+      </div>
     </div>
   );
 }
---
+++ src/buttonComponent.js
@@ -0,0 +1,8 @@
+import styles from './button.module.css';
+
+export default function Button(props) {
+    console.log(props);
+    return (
+        <button>Default</button>
+    )
+}
