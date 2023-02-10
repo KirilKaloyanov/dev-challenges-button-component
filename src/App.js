dcf345e0ffa9c9fc91c468e211c09cab6a0130c4
Kiril <kaloyanov.kiril@gmail.com>
2023-02-10 16:55:21 +0200

    Styled sizes and colors

--- src/App.js
+++ src/App.js
@@ -19,13 +19,62 @@ function App() {
       </div>
       <div>
         <div className={styles.label}>&lt;Button /&gt;</div>
-        <Button />
-        <div className={styles.label}>&lt;Button variant='outline' /&gt;</div>
-        <Button variant='outline'/>
-        <div className={styles.label}>&lt;Button variant='text' /&gt;</div>
-        <Button variant='text'/>
+        <Button label='Default' />
+
+        <div className={styles.label}>&lt;Button variant="outline" /&gt;</div>
+        <Button label='Default' variant='outline'/>
+
+        <div className={styles.label}>&lt;Button variant="text" /&gt;</div>
+        <Button label='Default' variant='text'/>
+
         <div className={styles.label}>&lt;Button disableShadow /&gt;</div>
-        <Button disableShadow/>
+        <Button label='Default' disableShadow/>
+
+        <div className={styles.smGrid}>
+          <div>
+            <div className={styles.label}>&lt;Button disabled /&gt;</div>
+            <Button label='Default' disabled />
+          </div>
+          <div>
+            <div className={styles.label}>&lt;Button variant="text" disabled /&gt;</div>
+            <Button label='Default' variant="text" disabled />
+          </div>
+        </div>
+
+        <div className={styles.grid}>
+          <div>
+            <div className={styles.label}>&lt;Button size="sm"/&gt;</div>
+            <Button label='Default'  color="Primary" size="sm"/>
+          </div>
+          <div>
+            <div className={styles.label}>&lt;Button size="md"/&gt;</div>
+            <Button label='Default'  color="Primary"/>
+          </div>
+          <div>
+            <div className={styles.label}>&lt;Button size="lg"/&gt;</div>
+            <Button label='Default'  color="Primary" size="lg"/>
+          </div>
+          <div></div>
+          
+          <div>
+            <div className={styles.label}>&lt;Button color="Default"/&gt;</div>
+            <Button label='Default' color="Default"/>
+          </div>
+          <div>
+            <div className={styles.label}>&lt;Button color="Primary"/&gt;</div>
+            <Button label='Default' color="Primary"/>
+          </div>
+          <div>
+            <div className={styles.label}>&lt;Button color="Secondary"/&gt;</div>
+            <Button label='Secondary' color="Secondary"/>
+          </div>
+          <div>
+            <div className={styles.label}>&lt;Button color="Danger"/&gt;</div>
+            <Button label='Danger' color="Danger"/>
+          </div>
+
+        </div>
+
         <footer><a href="https://devchallenges.io/portfolio/KirilKaloyanov" target={'_blank'} rel="noreferrer">created by <strong>KirilKaloyanov</strong> - devChallenges.io</a></footer>
       </div>
     </div>
--- src/buttonComponent.js
+++ src/buttonComponent.js
@@ -3,9 +3,23 @@ import './button.css';
 export default function Button(props) {
     let stylesString = 'default';
     if (props.color || props.variant) stylesString = '';
-    if (!props.disableShadow) stylesString +=' shadow';
+    
+    if (props.variant === 'outline') stylesString += 'outline';
+    if (props.variant === 'text') stylesString += 'text';
+    
+    if (props.color === 'Default') stylesString += 'default';
+    if (props.color === 'Primary') stylesString += 'primary';
+    if (props.color === 'Secondary') stylesString += 'secondary';
+    if (props.color === 'Danger') stylesString += 'danger';
+    
+    if (props.size === 'sm') stylesString += ' sm';
+    if (props.size === 'lg') stylesString += ' lg';
+
+    if (!(props.disableShadow || props.variant === 'text')) stylesString +=' shadow';
+
+    if (props.disabled) stylesString += ' disabled'
 
     return (
-        <button className={stylesString}>Default</button>
+        <button className={stylesString} disabled={props.disabled}>{props.label}</button>
     )
 }
